6e971d7e9a962c695eb69bc5d52d9039845b8f80
Daniel Bolivar herrera <dbolivarh@gmail.com>
2017-10-04 17:43:59 +0200

    Project Creation

---
+++ GitFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitFeed
+//
+//  Created by Daniel Bolivar herrera on 4/10/17.
+//  Copyright © 2017 Casumo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
